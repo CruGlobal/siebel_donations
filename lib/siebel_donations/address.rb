a181ea0d5b96f920aba44fd8526c31e2812c438e
Josh Starcher <josh.starcher@gmail.com>
2012-12-24 09:33:58 -0500

    Normalize address type

--- lib/siebel_donations/address.rb
+++ lib/siebel_donations/address.rb
@@ -1,9 +1,18 @@
 module SiebelDonations
   class Address < Base
 
-    attr_reader :id, :type, :primary, :seasonal, :address1, :address2, :address3, :address4, :city,
+    attr_reader :id, :primary, :seasonal, :address1, :address2, :address3, :address4, :city,
                 :state, :zip
 
+
+    def type
+      case @type
+      when 'Mailing Address'
+        'Mailing'
+      else
+        @type
+      end
+    end
   end
 end
 
