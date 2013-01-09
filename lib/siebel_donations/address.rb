16ae249a86b479651d719920a60cb7907319a0db
Josh Starcher <josh.starcher@gmail.com>
2013-01-09 11:31:59 -0500

    Mailing is a valid address type

--- lib/siebel_donations/address.rb
+++ lib/siebel_donations/address.rb
@@ -7,7 +7,7 @@ module SiebelDonations
 
     def type
       case @type.downcase
-      when 'mailing address', 'ship to', 'ma', 'mailng', 'm', 'billing', 'mailin', 'mailig'
+      when 'mailing address', 'ship to', 'ma', 'mailng', 'm', 'billing', 'mailin', 'mailig', 'mailing'
         'Mailing'
       else
         @type
